204a47a8093eb699efc5cbc202630836e748ca04
Luísa Barros <qlullyq@gmail.com>
2019-03-01 09:40:07 -0300

    Update main.js

--- spider-man-wiki/js/main.js
+++ spider-man-wiki/js/main.js
@@ -1,20 +1,8 @@
 (function(doc, win) {
     'use strict';
     
-    const topnavEl = doc.querySelector('#topnavClick');
-    const dropdownEl = doc.querySelector('#dropdownClick');
     const learnMoreBt = doc.querySelector('#learnMoreBt');
-
-    dropdownEl.addEventListener('click', dropdownMenu, false);
     learnMoreBt.addEventListener('click', openNewTab, false);
-
-    function dropdownMenu() {
-        if(topnavEl.className === "topnav") {
-        topnavEl.className += " responsive";
-        }else {
-            topnavEl.className = "topnav";
-        }
-    }
 
     function openNewTab() {
         event.preventDefault();
